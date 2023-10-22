fa851a1a8efa91820b46b3941c205b05d5c0253c
markj <mark.jani@web.de>
2023-10-22 12:55:21 +0200

    uebung

--- src/uebungen/uebung1.py
+++ src/uebungen/uebung1.py
@@ -2,8 +2,6 @@ import control as co
 import numpy as np
 import matplotlib.pyplot as plt
 
-s = co.tf('s')
-
 num = np.array([1])
 G_pt1 = np.array([5, 1])
 G_pt2a = np.array([5 * 0.05, 5 + 0.05, 1])
@@ -18,10 +16,10 @@ print(G_pt1_tf)
 print(G_pt2a_tf)
 print(G_I_tf)
 
-print('Poles von G_pt1: ' + str(co.poles(G_pt1_tf)))
-print('Zeros von G_pt1: ' + str(co.zeros(G_pt1_tf)))
-print('Poles von G_pt2a: ' + str(co.poles(G_pt2a_tf)))
-print('Zeros von G_pt2a: ' + str(co.zeros(G_pt2a_tf)))
+print('Poles von G_pt1:', co.poles(G_pt1_tf))
+print('Zeros von G_pt1:', co.zeros(G_pt1_tf))
+print('Poles von G_pt2a:', co.poles(G_pt2a_tf))
+print('Zeros von G_pt2a:', co.zeros(G_pt2a_tf))
 
 co.pzmap(G_pt2a_tf, plot=True)
 plt.show()
