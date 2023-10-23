1db0d5b03241227602c8c6602fed121b8d89104f
markj <mark.jani@web.de>
2023-10-23 16:05:31 +0200

    comments

--- src/uebungen/uebung1.py
+++ src/uebungen/uebung1.py
@@ -4,7 +4,7 @@ import control as co
 import numpy as np
 import matplotlib.pyplot as plt
 
-#num = Zähler, alle Folgenden Nenner
+#num = Zähler, alle folgenden Nenner
 num = np.array([1])
 G_pt1 = np.array([5, 1])
 G_pt2a = np.array([5 * 0.05, 5 + 0.05, 1])
@@ -17,13 +17,13 @@ G_pt2a_tf = co.tf(num, G_pt2a)
 G_pt2d_tf = co.tf(num, G_pt2d)
 G_I_tf = co.tf(num, G_I)
 
-# Alle Uebertragungsfunktionen ausgeben lassen
+# Alle Übertragungsfunktionen ausgeben lassen
 print(G_pt1_tf)
 print(G_pt2a_tf)
 print(G_pt2d_tf)
 print(G_I_tf)
 
-# Pol- und Nullstellen der Uebertragungsfunktionen ausgeben lassen
+# Pol- und Nullstellen der Übertragungsfunktionen ausgeben lassen
 print('Poles von G_pt1:', co.poles(G_pt1_tf))
 print('Zeros von G_pt1:', co.zeros(G_pt1_tf))
 print('Poles von G_pt2a:', co.poles(G_pt2a_tf))
@@ -33,7 +33,7 @@ print('Zeros von G_pt2d:', co.zeros(G_pt2d_tf))
 print('Poles von G_I:', co.poles(G_I_tf))
 print('Zeros von G_I:', co.zeros(G_I_tf))
 
-# Wenn ALLE realen Polstellen der Uebertragungsfunktion G_pt2a negativ sind, "Stabil!" ausgeben lassen
+# Wenn ALLE realen Polstellen der Übertragungsfunktion G_pt2a negativ sind, "Stabil!" ausgeben lassen
 if all(np.real(co.poles(G_pt2a_tf)) < 0):
     print("Stabil!")
 
@@ -51,7 +51,7 @@ co.bode(G_I_tf)
 plt.figure(4)
 co.nyquist_plot(G_I_tf)
 
-# Sprungsantwort der einzelnen Uebertragungsfuntkionen erstellen
+# Sprungantwort der einzelnen Uebertragungsfuntkionen erstellen
 G_pt1_step = co.step_response(G_pt1_tf)
 G_pt2a_step = co.step_response(G_pt2a_tf)
 G_pt2d_step = co.step_response(G_pt2d_tf)
