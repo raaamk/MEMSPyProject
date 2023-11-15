5cca35f4fb9295efe5a2b777e024d2abfbdabfc9
markj <mark.jani@web.de>
2023-11-15 12:54:16 +0100

    project

--- src/project/project1.py
+++ src/project/project1.py
@@ -62,13 +62,13 @@ l_R = 40
 
 # Generatorstrang
 n_G = 0.97
-J_0 = 6*10**7
-J_1 = 4*10**7
+J_0 = 6 * 10 ** 7
+J_1 = 4 * 10 ** 7
 J_Ges = J_0 + J_1 * 100
-b_0 = 1.2*10**7
-b_1 = 10.2*10**4
+b_0 = 1.2 * 10 ** 7
+b_1 = 10.2 * 10 ** 4
 b_Ges = b_0 + b_1 * 100
-i_G1 = 1/100
+i_G1 = 1 / 100
 
 # Windnachführung
 n_M = 0.95
@@ -82,7 +82,7 @@ T = 1  # Abtastzeit
 M_B = 0  # Bremsmoment
 M_G = 1000  # Windnachführung Motormoment
 iteration = 100  # Anzahl Iterationen
-w_d = 0 # Windrichtung in Grad
+w_d = 0  # Windrichtung in Grad
 
 # Output
 w = [0.0]
@@ -91,47 +91,51 @@ alpha_G_rad = [0.0]
 alpha_G_deg = [0.0]
 iteration_time = [0]
 
+
 # ----------------------------------
 # FUNCTIONS
 # ----------------------------------
+class WeatherDataFetcher:
+    def get_weather_data(self):
+        # Define API URL
+        api_url = "https://api.open-meteo.com/v1/forecast?latitude=50.93&longitude=6.95&current=temperature_2m,windspeed_10m,winddirection_10m"
+        # Request data
+        response = requests.get(api_url)
+        # Check if request and response is valid
+        windspeed_10m = []
+        winddirection_10m = []
+        if response.status_code == 200:
+            weather_data = response.json()
+            windspeed_10m = weather_data['current']['windspeed_10m']
+            winddirection_10m = weather_data['current']['winddirection_10m'] * math.pi / 180
+        else:
+            print("Failed to fetch weather data.")
 
-def get_weather_data(self):
-    # Define API URL
-    api_url = "https://api.open-meteo.com/v1/forecast?latitude=50.93&longitude=6.95&current=temperature_2m,windspeed_10m,winddirection_10m"
-    # Request data
-    response = requests.get(api_url)
-    # Check if request and response is valid
-    windspeed_10m = []
-    winddirection_10m = []
-    if response.status_code == 200:
-        weather_data = response.json()
-        windspeed_10m = weather_data['current']['windspeed_10m']
-        winddirection_10m = weather_data['current']['winddirection_10m']*math.pi/180
-    else:
-        print("Failed to fetch weather data.")
+        return windspeed_10m, winddirection_10m
 
-    return windspeed_10m, winddirection_10m
+    def save_weather_data(self):
+        # Hole die Wetterdaten
+        windspeed, winddirection = self.get_weather_data()
 
-
-
-
+        # Speichere die Wetterdaten in den Instanzvariablen
+        self.saved_windspeed = windspeed
+        self.saved_winddirection = winddirection
 
 # ----------------------------------
 # PREPROCESSING
 # ----------------------------------
 
 # Execute preprocessing operation
-#c_m = c_p[0]/la[0]
+# c_m = c_p[0]/la[0]
 v_w_R = v_w * math.cos(math.radians(w_d))
-
 
 # ----------------------------------
 # MAINPROCESSING
 # ----------------------------------
 
 # Execute main operation
-for i in range(1, iteration+1):
-    la_calc = l_R * w[i-1] / v_w
+for i in range(1, iteration + 1):
+    la_calc = l_R * w[i - 1] / v_w
 
     if la_calc < 5:
         la_calc = 5.0
@@ -141,14 +145,14 @@ for i in range(1, iteration+1):
     c_p_interp = np.interp(la_calc, la, c_p)
     c_m = c_p_interp / la_calc
 
-    weather = get_weather_data()
-    v_w = weather.w
-    v_w_R = v_w * math.cos(math.radians(w_d))
+    weatherdata = WeatherDataFetcher()
+    weatherdata.save_weather_data()
+    v_w = weatherdata.saved_windspeed
 
-    #w.append((c_m * 0.5 * rho_air * math.pi * l_R**3 * v_w_R**2 * T) / (J_0 + J_1 * 100) - w[i-1] * ((K_m * 100 * T) / (J_0 + J_1 * 100) + ((b_0 + b_1 * 100) * T) / (J_0 + J_1 * 100) - 1) - (M_B * T) / (J_0 + J_1 * 100))
-    w.append((0.5 * c_m * rho_air * math.pi * l_R**3 * v_w_R**2 * T) / J_Ges - (w[i - 1] * b_Ges * T) / J_Ges - (K_m * w[i - 1] * 100 * T) / J_Ges + w[i - 1] - (M_B * T) / J_Ges)
-    w_G.append((M_G * 1000 * T) / J_G - (b_G * w_G[i-1] * T) / J_G + w_G[i-1])
-    alpha_G_rad.append(w_G[i] * T + alpha_G_rad[i-1])
+    # w.append((c_m * 0.5 * rho_air * math.pi * l_R**3 * v_w_R**2 * T) / (J_0 + J_1 * 100) - w[i-1] * ((K_m * 100 * T) / (J_0 + J_1 * 100) + ((b_0 + b_1 * 100) * T) / (J_0 + J_1 * 100) - 1) - (M_B * T) / (J_0 + J_1 * 100))
+    w.append((0.5 * c_m * rho_air * math.pi * l_R ** 3 * v_w_R ** 2 * T) / J_Ges - (w[i - 1] * b_Ges * T) / J_Ges - (K_m * w[i - 1] * 100 * T) / J_Ges + w[i - 1] - (M_B * T) / J_Ges)
+    w_G.append((M_G * 1000 * T) / J_G - (b_G * w_G[i - 1] * T) / J_G + w_G[i - 1])
+    alpha_G_rad.append(w_G[i] * T + alpha_G_rad[i - 1])
     alpha_G_deg.append(math.degrees(alpha_G_rad[i]))
 
     if alpha_G_deg[i] - w_d > 20:
@@ -158,14 +162,13 @@ for i in range(1, iteration+1):
     else:
         M_G = 0
 
-    iteration_time.append(T*i)
+    iteration_time.append(T * i)
     print("Durchlauf", i)
     print("w:", w[i])
     print("la_calc:", la_calc)
 
-#print(w)
+# print(w)
 print("Winkel", math.degrees(alpha_G_rad[iteration]))
-
 
 # ----------------------------------
 # POSTPROCESSING
