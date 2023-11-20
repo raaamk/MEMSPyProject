a0f20cf8d621866dfbfd227273a4748c1d626ef8
markj <mark.jani@web.de>
2023-11-20 23:11:51 +0100

    project

--- src/project/project_windturbine.py
+++ src/project/project_windturbine.py
@@ -72,10 +72,10 @@ M_B = 0  # Bremsmoment
 M_G = 0  # Antriebsmoment Gondel
 
 # Input
-T = 0.01  # Zeit/Abtastrate
+T = 1  # Zeit/Abtastrate
 iteration = 300000  # Anzahl Iterationen
-v_w = 25.1  # Windgeschwindigkeit; Möglich eigenen Wert einzutippen, nur aktiv, wenn get_weather = False
-w_d = math.radians(0)  # Windrichtung; Möglich eigenen Wert einzutippen, nur aktiv, wenn get_weather = False
+v_w = 11  # Windgeschwindigkeit; Möglich eigenen Wert einzutippen, nur aktiv, wenn get_weather = False
+w_d = math.radians(180)  # Windrichtung; Möglich eigenen Wert einzutippen, nur aktiv, wenn get_weather = False
 get_weather = False  # Wenn True, aktuelle Winddaten werden verwendet
 
 # Output
