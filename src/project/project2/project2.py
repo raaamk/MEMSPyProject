c5d080912fb441a9c6a68dc7242fe48f59872e86
markj <mark.jani@web.de>
2023-11-29 23:39:43 +0100

    project 2

---
+++ src/project/project2/project2.py
@@ -0,0 +1,93 @@
+"""
+INFO
+Project 2
+
+INPUT VARIABLES
+-
+
+OUTPUT VARIABLES
+-
+
+DESCRIPTION
+-
+
+FILE
+.../project2.py
+
+ASSOCIATED FILES
+-
+
+AUTHOR(S)
+Mark Janitschek
+TH Köln / F09 / IPK / CAISA
+MEMS
+WS 2023/24
+
+DATE
+29.11.2023
+
+LAST MODIFIED
+29.11.2023
+
+Copyright 2023 - TH Köln
+"""
+# ----------------------------------
+# DEPENDENCIES
+# ----------------------------------
+
+import control as co
+import control.matlab as com
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# ----------------------------------
+# PARAMETERS
+# ----------------------------------
+
+# Definieren Sie die Übertragungsfunktion
+num = [8.3 * 10**(-8)]
+den = [5, 1]
+system = co.tf(num, den)
+
+# Zeitvektor (von 0 bis 30, in 0,01 Sekunden-Schritten)
+t = np.arange(0, 30, 0.01)
+
+# Eingangssignal definieren 1, solang wie t
+u = np.ones_like(t)
+
+# ----------------------------------
+# FUNCTIONS
+# ----------------------------------
+
+
+# ----------------------------------
+# PREPROCESSING
+# ----------------------------------
+
+
+# ----------------------------------
+# MAINPROCESSING
+# ----------------------------------
+
+# Main
+y, t_out, x_out = com.lsim(system, u, t)
+
+# Ausgangssignal mit Rauschen überlagern
+std_dev = 0.01 * np.max(y)  # Standardabweichung von 1 % des maximalen Ausgangswerts
+noise = np.random.normal(0, std_dev, y.shape) # Mittelwert des Rauschens ist 0, Standardabweichung ist das 1 % des max. Ausgangswertes, die Form ist wie die des Vektors y.
+y_with_noise = y + noise
+
+# ----------------------------------
+# POSTPROCESSING
+# ----------------------------------
+
+# Plotten Sie die Systemantwort mit Rauschen
+plt.plot(t_out, y_with_noise, label='Mit Rauschen')
+plt.plot(t_out, y, label='Ohne Rauschen', linestyle='--')
+plt.xlabel('Zeit')
+plt.ylabel('Systemantwort')
+plt.title('Systemantwort mit gaußverteiltem weißen Rauschen')
+plt.legend()
+plt.grid(True)
+plt.show()
