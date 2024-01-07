0ec680e8355f0dd289deb526fc3e63321e2f197c
markj <mark.jani@web.de>
2024-01-07 16:44:32 +0100

    project 3

--- src/project/project3/project3_ML.py
+++ src/project/project3/project3_ML.py
@@ -37,6 +37,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn.model_selection as skl_ms
 
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+import keras as k
+
 # ----------------------------------
 # PARAMETERS
 # ----------------------------------
@@ -55,7 +60,7 @@ num = [8.3 * 10 ** (-8)]
 den = [5, 1]
 
 system_c = co.tf(num, den, 0.01)
-#system_d = com.c2d(system_c, 0.01, method='zoh')
+# system_d = com.c2d(system_c, 0.01, method='zoh')
 
 
 # ----------------------------------
@@ -70,7 +75,6 @@ u = (sinus_signal + noise) * jump_array
 
 y, t_out, x_out = com.lsim(system_c, u, t)
 
-
 # ----------------------------------
 # AUFGABE 3
 # ----------------------------------
@@ -79,11 +83,10 @@ matrix = []
 nu = ny = 11
 
 for i in range(100 + ny, 3000):
-    new_line = [u[i], u[i-1], u[i-2], u[i-3], u[i-4], u[i-5], u[i-6], u[i-7], u[i-8], u[i-9], u[i-10], u[i-11], y[i], y[i-1], y[i-2], y[i-3], y[i-4], y[i-5], y[i-6], y[i-7], y[i-8], y[i-9], y[i-10], y[i-11]]
+    new_line = [u[i], u[i - 1], u[i - 2], u[i - 3], u[i - 4], u[i - 5], u[i - 6], u[i - 7], u[i - 8], u[i - 9], u[i - 10], u[i - 11], y[i], y[i - 1], y[i - 2], y[i - 3], y[i - 4], y[i - 5], y[i - 6], y[i - 7], y[i - 8], y[i - 9], y[i - 10], y[i - 11]]
     matrix.append(new_line)
 
 np.savetxt("matrix_aufgabe3_daten.csv", matrix, delimiter=',')
-
 
 # ----------------------------------
 # AUFGABE 4
@@ -91,38 +94,59 @@ np.savetxt("matrix_aufgabe3_daten.csv", matrix, delimiter=',')
 
 matrix_import = np.array(pd.read_csv('matrix_aufgabe3_daten.csv', sep=','))
 labels = matrix_import[:, 11]
-
+features = np.delete(matrix_import, 11, axis=1)
 
 # ----------------------------------
 # AUFGABE 5
 # ----------------------------------
 
-matrix_train, matrix_rest, y_train, y_rest = skl_ms.train_test_split(matrix_import, labels, test_size=0.3, train_size=0.7)
-matrix_val, matrix_test, y_val, y_test = skl_ms.train_test_split(matrix_rest, y_rest, test_size=0.33, train_size=0.67)
+X_train, X_rest, y_train, y_rest = skl_ms.train_test_split(features, labels, test_size=0.3, train_size=0.7)
+X_val, X_test, y_val, y_test = skl_ms.train_test_split(X_rest, y_rest, test_size=0.33, train_size=0.67)
 
 
 # ----------------------------------
 # AUFGABE 6
 # ----------------------------------
 
+model = k.Sequential(
+    [
+        k.layers.Normalization(axis=nu+ny, name="layer1"),
+        k.layers.Dense(16, name="layer2"),
+        k.layers.Dense(64, activation='sigmoid', name="layer3"),
+        k.layers.Dense(8, name="layer4"),
+    ]
+)
 
 # ----------------------------------
 # AUFGABE 7
 # ----------------------------------
 
+adam_opt = k.optimizers.Adam(learning_rate=0.001)
+model.compile(optimizer=adam_opt, loss='mean_squared_error', metrics=['mean_squared_error'])
+
+model.summary()
 
 # ----------------------------------
 # AUFGABE 8
 # ----------------------------------
 
+callback = k.callbacks.EarlyStopping(monitor='loss', patience=100)
+
+model.fit(
+    x=X_train,
+    y=y_train,
+    batch_size=64,
+    epochs=500,
+    verbose='auto',
+    callbacks=[callback],
+    validation_data=y_val.all()
+)
+
+model.summary()
+
 
 # ----------------------------------
 # AUFGABE 9
-# ----------------------------------
-
-
-# ----------------------------------
-# AUFGABE 6
 # ----------------------------------
 
 
